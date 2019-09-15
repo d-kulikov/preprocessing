cb7a7bd2f777052b63e7b46c62ca10e13567c303
d-kulikov <dmitkulik@gmail.com>
2019-09-15 12:20:02 +0300

    Binaries function added

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Functions for detection and processing outliers. The input is the whole dataset,
 and processed automatically. Extreme values are replaced with threshold values. Based on the 1.5-IQR rule for symmetrical 
 (normal-like) distributions but modified for skewed (non-negative) distributions where outliers are only one-sided. Works
 also for very dense distributions (with high predominance of some one or several values).
+
+## binaries
+A function that restores integer dummy values of binary variables after scaling (e.g., after applying sklearn's StandardScaler).
+Otherwise, if they are very impalanced, they may have extremely high values (this is not desired for optimization algorithms). 
+Accepts the whole numpy array as input and processes only binary variables. The function can not only restore original 0 and 1
+values, but, depending on the distribution, return 0 and -1 or 1 and -1. This allows to keep the mean closer to 0 and the
+standard deviation to 1 (as with truly numeric variables).
---
+++ binaries.py
@@ -0,0 +1,53 @@
+def binaries( reference_nparray, nparray_to_transform ) :
+    
+    """ Restores integer dummy values of binary variables after scaling (e.g., after applying sklearn's StandardScaler).
+    Otherwise, if they are very impalanced, they may have extremely high values (this is not desired for optimization algorithms). 
+    Accepts the whole numpy array as input and processes only binary variables. The function can not only restore original 0 and 1
+    values, but, depending on the distribution, return 0 and -1 or 1 and -1. This allows to keep the mean closer to 0 and the
+    standard deviation to 1 (as with truly numeric variables).
+    
+    Example:
+    X_TRAIN_RESTORED = binaries( X_TRAIN, X_TRAIN )
+    X_TEST_RESTORED = binaries( X_TRAIN, X_TEST  )
+    """
+    
+    import numpy as np
+    
+    from scipy.stats import mode
+    
+    Nrows = reference_nparray.shape[ 0 ]
+    
+    # Creating a new array based on the array we want to modify
+    OUT = nparray_to_transform
+    
+    for i in range( 0, reference_nparray.shape[ 1 ] ) :
+        
+        # Checks if this is a binary variable
+        if len( np.unique( reference_nparray[ :, i ] ) ) == 2 :
+            
+            Min = np.min( reference_nparray[ :, i ] )
+            
+            Max = np.max( reference_nparray[ :, i ] )
+            
+            Mode = mode( reference_nparray[ :, i ] )[ 0 ]
+            
+            # Checks if the distribution is imbalanced
+            if sum( reference_nparray[ :, i ] == Mode ) / Nrows >= 2/3 :
+                
+                # The most frequent value becomes 0 
+                if Mode == Min :
+                
+                    OUT[ :, i ] = np.where( OUT[ :, i ] == Mode, 0, 1 )
+                    
+                else:
+                    
+                    OUT[ :, i ] = np.where( OUT[ :, i ] == Mode, 0, -1 )
+                
+            else :
+                
+                # If the values are more or less balanced, they become -1 and 1
+                OUT[ OUT[ :, i ] == Min, i ] = -1
+                 
+                OUT[ OUT[ :, i ] == Max, i ] = 1
+                
+    return OUT
