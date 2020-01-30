57b4905a160353c84ec3376b03cd60e1b1de494d
d-kulikov <dmitkulik@gmail.com>
2020-01-30 16:49:29 +0300

    binaries.py: Class definition added

--- binaries.py
+++ binaries.py
@@ -1,7 +1,8 @@
-def binaries( reference_nparray, nparray_to_transform ) :
-    
-    """ ***************************************************************************************************************************
-    Restores integer dummy values of binary variables after scaling (e.g., after applying sklearn's StandardScaler).
+import numpy as np
+from scipy.stats import mode
+
+class Binaries( object ) :
+        """ Restores integer dummy values of binary variables after scaling (e.g., after applying sklearn's StandardScaler).
     Otherwise, if they are very impalanced, they may have extremely high values (this is not desired for optimization algorithms). 
     Accepts the whole numpy array as input and processes only binary variables. The function can not only restore original 0 and 1
     values, but, depending on the distribution, return 0 and -1 or 1 and -1. This allows to keep the mean closer to 0 and the
@@ -12,43 +13,37 @@ def binaries( reference_nparray, nparray_to_transform ) :
     X_TEST_RESTORED = binaries( X_TRAIN, X_TEST  )
     """
     
-    import numpy as np
-    
-    from scipy.stats import mode
-    
-    Nrows = reference_nparray.shape[ 0 ]
-    
-    # Creating a new array based on the array we want to modify
-    OUT = nparray_to_transform
-    
-    for i in range( 0, reference_nparray.shape[ 1 ] ) :
+    def _init_( self ) :
+        self.values = None
+        self.ncols = 0
         
-        # Checks if this is a binary variable
-        if len( np.unique( reference_nparray[ :, i ] ) ) == 2 :
-            
-            Min = np.min( reference_nparray[ :, i ] )
-            
-            Max = np.max( reference_nparray[ :, i ] )
-            
-            Mode = mode( reference_nparray[ :, i ] )[ 0 ]
-            
-            # Checks if the distribution is imbalanced
-            if sum( reference_nparray[ :, i ] == Mode ) / Nrows >= 2/3 :
-                
-                # The most frequent value becomes 0 
-                if Mode == Min :
-                
-                    OUT[ :, i ] = np.where( OUT[ :, i ] == Mode, 0, 1 )
+    def fit( self, X ) :
+        nrows = X.shape[ 0 ]        
+        self.ncols = X.shape[ 1 ]        
+        # Creating an array of the parameters
+        self.values = np.full( ( 2, self.ncols ), fill_value=np.nan, dtype=np.float )        
+        for i in range( 0, self.ncols ) :
+            # Checks if this is a binary variable
+            if len( np.unique( X[ :, i ] ) ) == 2 :    
+                minim = np.min( X[ :, i ] )    
+                maxim = np.max( X[ :, i ] )    
+                mode_value = mode( X[ :, i ] )[ 0 ]    
+                # Checks if the distribution is imbalanced
+                if sum( X[ :, i ] == mode_value ) / nrows >= 2/3 :        
+                    # The most frequent value becomes 0 
+                    if mode_value == minim :
+                        self.values[ 1, i ] = 0
+                        self.values[ 0, i ] = 1
+                    else:
+                        self.values[ 1, i ] = -1
+                        self.values[ 0, i ] = 0
+                else :
+                    # If the values are more or less balanced, they become -1 and 1
+                    self.values[ 1, i ] = -1; self.values[ 0, i ] = 1
                     
-                else:
-                    
-                    OUT[ :, i ] = np.where( OUT[ :, i ] == Mode, 0, -1 )
-                
-            else :
-                
-                # If the values are more or less balanced, they become -1 and 1
-                OUT[ OUT[ :, i ] == Min, i ] = -1
-                 
-                OUT[ OUT[ :, i ] == Max, i ] = 1
-                
-    return OUT
+    def predict( self, X ) :
+        for i in range( 0, self.ncols ) :    
+            # Checks if this is a binary variable
+            if np.isnan( self.values[ 0, i ] ) == False :
+                X[ :, i ] = np.where( X[ :, i ] > np.mean( X[ :, i ] ), self.values[ 0, i ], self.values[ 1, i ] )
+        return X
