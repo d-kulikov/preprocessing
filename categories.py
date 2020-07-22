d83ddcd7d3db7d4bedc7aa67cb2c76df28965ec2
d-kulikov <dmitkulik@gmail.com>
2020-07-22 22:13:37 +0300

    Categories

--- categories.py
+++ categories.py
@@ -1,25 +1,25 @@
 import numpy as np
 import pandas as pd
 
-class Levels( object ) :
+class Categories( object ) :
     
-    """ A function for transforming categorical variables (especially helpful for those with a huge number of levels, e.g. car model, city, zip ) into numeric
+    """ A function for transforming categorical variables (especially helpful for those with a huge number of categories, e.g. car model, city, zip ) into numeric
     ones. Each level of the variable will correspond to a certain continuous value based on the distribution of the target variable within this level.
     It works for both regression and binary classification problems.
     
     features: a list of feature names to transform
     label: the name of a target binary variable in the dataframe (continuous or binary)
     sample_frac: the share of the observations to sample from the input dataset (using not the whole dataset should reduce overfit)
-    dummy_number: a number of dummy observations equal to the average added to each level group in order to mitigate the effect of rare levels (levels with a few
+    dummy_number: a number of dummy observations equal to the average added to each level group in order to mitigate the effect of rare categories (categories with a few
                   data points will just become close to the general average )
     
     Example:    
     d = pd.DataFrame( { 'a' : [ 'i', 'i', 'i', 'k', 'k', 'k', 'l', 'l', 'l', 'm', 'm', 'm' ],
                         'b' : [ 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1 ] } )
-    levels = Levels( features=[ 'a' ], label='b', sample_frac=0.7, dummy_number=1 )
-    levels.fit( d )    
-    print( levels.vars_[ 'a' ] )
-    transformed = levels.transform( d ) """
+    categories = Categories( features=[ 'a' ], label='b', sample_frac=0.7, dummy_number=1 )
+    categories.fit( d )
+    print( categories.vars_[ 'a' ] )
+    transformed = categories.transform( d ) """
     
     def __init__( self, features, label, sample_frac=0.8, dummy_number=30 ) :
         self.features_ = features
@@ -49,4 +49,3 @@ class Levels( object ) :
         for name in self.features_ :
             out[ name ] = np.where( out[ name ].isin( self.vars_[ name ].index ), self.vars_[ name ][ 'value' ][ out[ name ] ], np.nan )
         return out
-            
